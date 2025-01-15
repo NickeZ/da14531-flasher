b8dac9ff1c67ce47aa67a811223192964b2488fa
Niklas Dusenlund <niklas.dusenlund@bitbox.swiss>
2025-01-15 14:37:04 +0100

    rewrote with asyncio

--- simulator.py
+++ simulator.py
@@ -1,49 +1,11 @@
-import serial
 import sys
-from enum import Enum
 import time
-import threading
-import socket
-import selectors
+import asyncio
+import serial_asyncio
 
-# Respond with SOH(0x01), len (LSB), len(MSB)
-def send_header(ser, length):
-    if length > 0xffff:
-        print("too large firmware to flash")
-        return
-    print(f"bytes to send {length}")
-    ser.write(b"\x01")
-    out = length.to_bytes(2, byteorder="little")
-    ser.write(out)
-
-def scan_byte(ser, byte):
-    for i in range(5):
-        r = read_byte(ser)
-        if r == byte:
-            return True
-    return False
-
-def scan_result(ser, ok, err):
-    for i in range(5):
-        r = read_byte(ser)
-        if r == ok:
-            return True
-        elif r == err:
-            return False
-        else:
-            print(f"dropping [{r:X}]")
-    raise Exception("Neither ok, nor err seen")
-
-def read_byte(ser):
-    # 10 attempts
-    for i in range(10):
-        r = ser.read(1)
-        if len(r) == 0:
-            print("timeout");
-            continue
-        print(f"Read data: [{r[0]:X}]");
-        return r[0]
-    raise Exception("Ran out of attempts")
+def usage():
+    print("Script to connect uart to simulator, can optionally flash the device over uart as well")
+    print("usage: main.py <SERIAL_PORT> [FILENAME]")
 
 # Checksum is all bytes XOR'd
 def checksum(bs):
@@ -52,54 +14,71 @@ def checksum(bs):
         chk ^= b
     return chk
 
-class LoaderState(Enum):
-    IDLE = 0
-    HEADER_WAIT_ACK = 1
-    HEADER_ACKED = 2
-    FIRMWARE_WAIT_CHK = 3
-    FIRMWARE_CHK_GOOD = 4
+async def flash_firmware(reader, writer, firmware):
+    length = len(firmware)
+    if length > 0xffff:
+        raise Exception("too large firmware to flash")
 
-def usage():
-    print("Script to connect uart to simulator, can optionally flash the device over uart as well")
-    print("usage: main.py <SERIAL_PORT> [FILENAME]")
+    # 0x02 = STX
+    await reader.readuntil(b"\x02")
+    print(f"bytes to send {length}")
 
-def flash_firmware(ser):
-    state = LoaderState.IDLE
+    # 0x01 = SOH
+    writer.write(b"\x01")
+    await writer.drain()
+
+    # len (LSB,MSB)
+    out = length.to_bytes(2, byteorder="little")
+    writer.write(out)
+    await writer.drain()
+
+    # 0x06 = ACK
+    # 0x15 = ERR
+    res = await reader.read(1)
+    if res != b"\x06":
+        raise Exception(f"Didn't ack header {res}")
+
+    # Send fimrware
+    now = time.time()
+    writer.write(firmware)
+    await writer.drain()
+
+    # Check checksum
+    res = await reader.read(1)
+    if res[0] != checksum(firmware):
+        raise Exception(f"Checksum failed, read {res}")
+
+    # Ack checksum
+    writer.write(b"\x06")
+    await writer.drain()
+
+    # Device booted successfully!
+
+def print_packet(buf):
+    trimmed_buf = buf.rstrip(b"\00")
+    print(f"{trimmed_buf.hex()}", end="")
+    if len(buf) != len(trimmed_buf):
+        print("[0...]")
+    else:
+        print()
+
+async def forward_uart(serial, socket):
     while True:
-        if state == LoaderState.IDLE:
-            # 0x02 = STX
-            if scan_byte(ser, 0x02):
-               print("saw STX, sending header")
-               send_header(ser, len(firmware))
-               state = LoaderState.HEADER_WAIT_ACK
-        if state == LoaderState.HEADER_WAIT_ACK:
-            # 0x06 = ACK
-            # 0x15 = ERR
-            if scan_result(ser, 0x06, 0x15):
-                print("saw ack header")
-                state = LoaderState.HEADER_ACKED
-            else:
-                print("saw error")
-                state = LoaderState.IDLE
-        if state == LoaderState.HEADER_ACKED:
-            print("sending firmware")
-            now = time.time()
-            ser.write(firmware)
-            print(f"took {time.time()-now:.2f}s")
-            state = LoaderState.FIRMWARE_WAIT_CHK
-        if state == LoaderState.FIRMWARE_WAIT_CHK:
-            if read_byte(ser) == checksum(firmware):
-                state = LoaderState.FIRMWARE_CHK_GOOD
-            else:
-                state = LoaderState.IDLE
-                print("failure, will wait for STX again")
-        if state == LoaderState.FIRMWARE_CHK_GOOD:
-            # 0x06 = ACK
-            ser.write(b"\x06")
-            print("success");
-            break
+        buf = await serial.readexactly(64)
+        print("read from serial")
+        print_packet(buf)
+        socket.write(buf)
+        await socket.drain()
 
-def main():
+async def forward_socket(serial, socket):
+    while True:
+        buf = await socket.readexactly(64)
+        print(f"read from socket")
+        print_packet(buf)
+        serial.write(buf)
+        await serial.drain()
+
+async def main():
     if len(sys.argv) < 2 or len(sys.argv) > 3:
         usage()
         return 1
@@ -107,92 +86,20 @@ def main():
     if len(sys.argv) == 3:
         with open(sys.argv[2], mode='rb') as fh:
             firmware = fh.read()
-    ser = serial.Serial(sys.argv[1], 115200, timeout=1, rtscts=True)
-    #ser.rts = False
-    if firmware != None:
-        flash_firmware(ser, firmware)
 
-    # drain any extra bytes
-    ser.read()
+    ser_reader, ser_writer = await serial_asyncio.open_serial_connection(url=sys.argv[1], baudrate=115200)
+
+    if firmware != None:
+        await flash_firmware(ser_reader, ser_writer, firmware)
 
     print("will forward all uart comms to simulator now")
+    sock_reader, sock_writer = await asyncio.open_connection(host="localhost", port=15423)
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("localhost", 15423))
+    task1 = asyncio.create_task(forward_uart(ser_reader, sock_writer))
+    task2 = asyncio.create_task(forward_socket(ser_writer, sock_reader))
 
-    sel = selectors.DefaultSelector()
-    sel.register(ser, selectors.EVENT_READ)
-    sel.register(s, selectors.EVENT_READ)
-
-    request = b""
-    keep_going = True
-    responses = []
-    while keep_going:
-        try:
-            events = sel.select()
-            for key, mask in events:
-                if key.fileobj is ser:
-                    if mask & selectors.EVENT_WRITE:
-                        print(f"UART: ready to write")
-                        if not responses:
-                            print("UART: Error: Nothing to respond with...")
-                        for response in responses:
-                            print(f"UART: writing ({response.hex()})")
-                            bytes_written = ser.write(response)
-                            print(f"UART: wrote ({bytes_written})")
-                        # After a packet is forwarded, listen again
-                        responses = []
-                        sel.modify(ser, selectors.EVENT_READ)
-                    if mask & selectors.EVENT_READ:
-                        print(f"UART: ready to read")
-                        r = ser.read()
-                        if len(r) == 0:
-                            # async timeout
-                            print("BUG: does this happen?")
-                            return
-
-                        request += r
-                        if len(request) != 64:
-                            print(f"UART: read {len(r)} bytes")
-                            continue
-                        print(f"UART: read request ({len(request)}) {request.hex()}")
-                        # We have read a full 64 byte packet from the serial
-                        # device, register the intent to write it to the socket
-                        sel.modify(s, selectors.EVENT_WRITE)
-                if key.fileobj is s:
-                    if mask & selectors.EVENT_WRITE:
-                        print("TCP: ready to write")
-                        print(f"TCP: writing  {request.hex()}")
-                        if s.sendall(request) != None:
-                            print("TCP: Error: tcp comms", file=sys.stderr)
-                        print(f"TCP: wrote {len(request)} bytes")
-                        request = b""
-                        # After a packet is forwarded, listen again
-                        sel.modify(s, selectors.EVENT_READ)
-                    if mask & selectors.EVENT_READ:
-                        print("TCP: ready to read")
-                        response = s.recv(64)
-                        if len(response) == 0:
-                            print("TCP: Network connection to simulator closed")
-                            # Socket closed
-                            keep_going = False
-                            break
-                        if len(response) != 64:
-                            print("BUG: need to handle partial packets...")
-                        print(f"TCP: read ({len(response)}) {response.hex()}")
-                        responses.append(response)
-                        # We have read a full 64 byte packet from the socket
-                        # register the intent to write it to the serial device
-                        sel.modify(ser, selectors.EVENT_WRITE)
-        except Exception as e:
-            print(f"{e}")
-            break
-    sel.unregister(s)
-    sel.unregister(ser)
-    sel.close()
-    ser.close()
-    s.close()
-
+    await task1
+    await task2
 
 if __name__=="__main__":
-    sys.exit(main())
+    sys.exit(asyncio.run(main()))
